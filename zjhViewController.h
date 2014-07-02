6b9646589b08c90eec86793ccbd148beb72f822f
danio <danio@daniotekiMacBook-Air.local>
2014-07-02 22:36:52 +0800

    add view controller

---
+++ zjhViewController.h
@@ -0,0 +1,13 @@
+//
+//  zjhViewController.h
+//  uicontrol
+//
+//  Created by danio on 14-7-2.
+//  Copyright (c) 2014年 danio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface zjhViewController : UIViewController
+
+@end
---
+++ zjhViewController.m
@@ -0,0 +1,38 @@
+//
+//  zjhViewController.m
+//  uicontrol
+//
+//  Created by danio on 14-7-2.
+//  Copyright (c) 2014年 danio. All rights reserved.
+//
+
+#import "zjhViewController.h"
+
+@interface zjhViewController ()
+
+@end
+
+@implementation zjhViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
