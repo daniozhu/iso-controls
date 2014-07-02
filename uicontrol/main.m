2fe543bb13173c08a350854b8f81f1abc8607bd8
danio <danio@daniotekiMacBook-Air.local>
2014-07-02 21:00:32 +0800

    Initial Commit

---
+++ uicontrol/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uicontrol
+//
+//  Created by danio on 14-7-2.
+//  Copyright (c) 2014年 danio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "zjhAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([zjhAppDelegate class]));
+    }
+}
---
+++ uicontrol/zjhAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  zjhAppDelegate.h
+//  uicontrol
+//
+//  Created by danio on 14-7-2.
+//  Copyright (c) 2014年 danio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface zjhAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
