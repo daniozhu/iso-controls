fa066df40e2a9c261a69563f43c74c547a56c4b6
danio <danio@daniotekiMacBook-Air.local>
2014-07-06 11:08:24 +0800

    add segment control and slider bar

--- uicontrol/zjhControlFunViewController.m
+++ uicontrol/zjhControlFunViewController.m
@@ -9,12 +9,29 @@
 #import "zjhControlFunViewController.h"
 
 @interface zjhControlFunViewController ()
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLable;
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 
 @end
 
 @implementation zjhControlFunViewController
+
+- (IBAction)toggleControls:(UISegmentedControl *)sender {
+}
+
+- (IBAction)switchChanged:(UISwitch *)sender {
+    BOOL setting = sender.isOn;
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+
+- (IBAction)sliderChanged:(UISlider *)sender {
+    int progress = lroundf(sender.value);
+    self.sliderLable.text=[NSString stringWithFormat:@"%d",progress];
+}
 
 - (IBAction)backgroudTap:(id)sender {
     [self.nameField resignFirstResponder];
@@ -38,6 +55,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.sliderLable.text = @"50";
 }
 
 - (void)didReceiveMemoryWarning
